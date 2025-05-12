ddbcad2cfaea7bb378e14e7733b70bfd1cc1b792
YJ Soon <yjsoon@tinkertanker.com>
2025-05-12 14:29:56 +0800

    Added tabs for 2 of 3 tabs

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createStaticNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 // Import local screens
 import HomeScreen from "./screens/HomeScreen";
@@ -12,7 +13,24 @@ const Tabs = createBottomTabNavigator({
     Home: HomeScreen,
     Events: EventsScreen,
     Contact: ContactScreen
-  }
+  },
+  screenOptions: ({ route }) => ({
+    tabBarActiveTintColor: "blue",
+    tabBarInactiveTintColor: "gray",
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+
+      if (route.name === "Home") {
+        iconName = "home";
+      } else if (route.name === "Events") {
+        iconName = "calendar";
+      } else {
+        iconName = "question-circle";
+      }
+
+      return <FontAwesome name={iconName} size={size} color={color} />;
+    }
+  })
 });
 
 // Root navigation component -------------------------------
